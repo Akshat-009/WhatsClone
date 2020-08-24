1d841a7783175c73ddf0d4f94af59e24634bad90
akshat <akshat_g@ch.iitr.ac.in>
2020-08-25 01:54:25 +0530

    Added button for new Chat

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 import SideBar from './SideBar'
+import Chat from './Chat'
 function App() {
   return (
     <>
       <div className="app">
         <div className="app__body">
           <SideBar/>
+          <Chat/>
         </div>
       </div>
     </>
---
+++ src/Chat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Chat.css'
+function Chat() {
+    return (
+        <div className="conversation">
+            
+        </div>
+    )
+}
+
+export default Chat
