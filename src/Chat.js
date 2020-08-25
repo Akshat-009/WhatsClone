5d656f2f00fc4fd6987945d34503567f38e1bcd1
akshat <akshat_g@ch.iitr.ac.in>
2020-08-25 17:34:57 +0530

    Added message footer

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import './Chat.css'
 import Avatar from '@material-ui/core/Avatar';
 import SearchIcon from '@material-ui/icons/Search';
 import IconButton from '@material-ui/core/IconButton';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
 function Chat() {
+const [input, setinput] = useState("")
+const sendmessage =(e)=>{
+    e.preventDefault()
+    console.log(input)
+    setinput("")
+}
     return (
         <div className="conversation">
             <div className="conv__header">
@@ -29,12 +37,17 @@ function Chat() {
             <div className="conv__body">
                 
             <p className="message"><span className="sender">Akshat</span>sample message<span className="timestamp">3:52 pm</span></p>
-            <p className="message message_recieve"><span className="sender">Akshat</span>sample message</p>
+            <p className={`message ${true && "message_recieve"}`}><span className="sender">Akshat</span>sample message</p>
             <p className="message"><span className="sender">Akshat</span>sample message</p>
         
             </div>
             <div className="conv__footer">
-            
+                <IconButton><InsertEmoticonIcon/></IconButton>
+             <form>
+                 <input value={input} onChange={(e)=>{setinput(e.target.value)}}  type="text" placeholder="Enter Your Message"></input>
+                 <button onClick={sendmessage} type="submit">Send message</button>
+             </form>
+            <IconButton><MicIcon/></IconButton>
             </div>
         </div>
     )
